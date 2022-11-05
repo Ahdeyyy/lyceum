df0f4dd60d8d63a70d3820bed72433015a9cb213
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-11-05 21:25:56 +0100

    feature: submit button modal

---
+++ src/lib/components/question/submitButton.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let func;
+</script>
+
+<label for="my-modal-3" class="btn btn-primary my-2 w-full ">Submit</label>
+
+<input type="checkbox" id="my-modal-3" class="modal-toggle" />
+<div class="modal">
+	<div class="modal-box relative">
+		<label for="my-modal-3" class="btn btn-primary btn-sm btn-circle absolute right-2 top-2"
+			>✕</label
+		>
+		<p class="py-4">Do you want to submit your answers?</p>
+		<div class="modal-action">
+			<button for="my-modal-3" class="btn btn-success my-2 mx-3" on:click={func}>Yes</button>
+		</div>
+	</div>
+</div>
