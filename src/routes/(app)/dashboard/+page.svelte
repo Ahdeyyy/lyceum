33b84daf37028e66d7a63932096946740aeb7118
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-11-06 16:40:26 +0100

    admin dashboard

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,4 +1,12 @@
 <script>
     /** @type {import('./$types').PageData} */
-    export let data;
 </script>
+
+<div class="flex flex-col space-y-10">
+    
+    <a class="btn btn-ghost" href="/dashboard/courses">Courses</a>
+    <a class="btn btn-ghost"  href="/dashboard/new_course">Add new course</a>
+    <a class="btn btn-ghost" href="/dashboard/questions">Questions</a>
+    <a class="btn btn-ghost" href="/dashboard/new_question">Add new question</a>
+
+</div>
