d9921eaeab2893b17dab1d500b3080a4bedf621d
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-11-06 21:58:16 +0100

    courses route in the dashboard

---
+++ src/routes/(app)/dashboard/courses/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<div class="flex flex-col">
+    {#each data.courses.items as item}
+        <a href="/courses/{item.slug}" class="text-xl mx-2 py-10 px-3 shadow-lg rounded-md" >
+            {item.name} :
+            {item.code}
+        </a>     
+    {/each}
+</div>
