2d43d8e172454f202cbed18104fceb1aed41829f
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-11-07 11:30:57 +0100

    refactored ui

--- src/routes/(app)/courses/+page.svelte
+++ src/routes/(app)/courses/+page.svelte
@@ -1,14 +1,16 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+	/** @type {import('./$types').PageData} */
+	export let data;
 </script>
 
-<div class="flex flex-col">
-    
-    {#each data.courses.items as item}
-         <a href="/courses/{item.slug}" class="text-xl mx-2 py-10 px-3 shadow-lg rounded-md" >
-             {item.name} :
-             {item.code}
-         </a>
-    {/each}
+<div class="grid md:grid-cols-3 auto-cols-max auto-rows-max ">
+	{#each data.courses.items as item}
+		<a
+			href="/courses/{item.slug}"
+			class="text-xl mx-2 py-10 px-3 shadow-lg rounded-md hover:scale-105"
+		>
+			{item.name} :
+			{item.code}
+		</a>
+	{/each}
 </div>
