addd3f8cd95ceb9a7802a5a88c7eb06c3cbe0eee
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-11-09 19:09:38 +0100

    refactored the pages

--- src/routes/(app)/courses/+page.js
+++ src/routes/(app)/courses/+page.js
@@ -3,14 +3,13 @@ import PocketBase from 'pocketbase';
 const client = new PocketBase('https://lag-pq.fly.dev');
 
 /** @type {import('./$types').RequestHandler} */
-export async function load () {
-    
-    const courses = await client.records.getList('courses', 1, 50, {
-        filter: 'created >= "2022-01-01 00:00:00"',
-    });
+export async function load() {
+	// list of all courses
+	const courses = await client.records.getList('courses', 1, 100, {
+		filter: 'created >= "2022-01-01 00:00:00"'
+	});
 
-    // console.log(courses);
-    return {
-        courses: courses,
-    };
+	return {
+		courses: courses
+	};
 }
--- src/routes/(app)/courses/[slug]/+page.js
+++ src/routes/(app)/courses/[slug]/+page.js
@@ -4,28 +4,31 @@ const client = new PocketBase('https://lag-pq.fly.dev');
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
+	// course based on slug
 	const course = await client.records.getList('courses', 1, 1, {
 		filter: `slug = "${params.slug}"`
 	});
-    if (course.items[0] !== undefined ) {
-        const resultList = await client.records.getList('questions', 1, 50, {
-            filter: `course = "${course.items[0].id}"`
-        });
+	if (course.items[0] !== undefined) {
+		// filter questions by course
+		let resultList = await client.records.getList('questions', 1, 200, {
+			filter: `course = "${course.items[0].id}"`
+		});
 
-        // loop throught the result list and then get the options
-        for (let i = 0; i < resultList.items.length; i++) {
+		// randomize questions and limit to 50
+		resultList.items = resultList.items.sort(() => Math.random() - 0.5).slice(0, 50);
 
-            const options = await client.records.getList('options', 1, 5, {
-                filter: `question = "${resultList.items[i].id}"`
-            });
+		// loop throught the result list and then get the options for each question
+		for (let i = 0; i < resultList.items.length; i++) {
+			const options = await client.records.getList('options', 1, 5, {
+				filter: `question = "${resultList.items[i].id}"`
+			});
 
-            resultList.items[i].options = options.items;
-        }
+			resultList.items[i].options = options.items;
+		}
 
-        return {
-            questions: resultList,
-            course: course.items[0]
-        };
-
-    }
+		return {
+			questions: resultList,
+			course: course.items[0]
+		};
+	}
 }
